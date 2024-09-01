fbf60800e51ecd4e9aac2b0c1f5fc4e9804c9702
Trijal <123671129+trijal18@users.noreply.github.com>
2024-09-02 03:26:32 +0530

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from wms_images import wms_images
-from combine import create_video
-
-wms_url="https://firms.modaps.eosdis.nasa.gov/mapserver/wms/fires/YourMapKey/fires_viirs_24/?REQUEST=GetMap&WIDTH=1024&HEIGHT=512&BBOX=-180,-90,180,90"
-wms_images(wms_url)
-create_video('images', 'output_video.mp4', 4)
